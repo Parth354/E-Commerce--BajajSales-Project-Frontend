e44082620dfa5f0f36baeb67eeeef0a408484e1c
Parth Bajaj <120926651+Parth354@users.noreply.github.com>
2024-03-11 14:32:42 +0530

    Update Logo.jsx

--- BajajSales/src/components/Logo.jsx
+++ BajajSales/src/components/Logo.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 function Logo() {
   return (
-    <div><img src="./src/assets/bajajsales1.png" alt='Bajaj Sales' style={{height:"40px", width:"155px"}}></img></div>
+    <div><img src={"./src/assets/bajajsales1.png"|| "/bajajsales1.png"} alt='Bajaj Sales' style={{height:"40px", width:"155px"}}></img></div>
   )
 }
 
