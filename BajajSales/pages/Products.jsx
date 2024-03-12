b55d37ffb2b4336d27b79975cbe7f2069cd8d923
Parth Bajaj <120926651+Parth354@users.noreply.github.com>
2024-03-12 17:28:37 +0530

    changed

--- BajajSales/pages/Products.jsx
+++ BajajSales/pages/Products.jsx
@@ -10,7 +10,7 @@ export default function AllProduct() {
     useEffect(() => {
         const getDetails = async () => {
             try {
-                const response = await fetch("http://localhost:8000/bajajsales/product-details", {
+                const response = await fetch("https://bajaj-sales-backend.onrender.com/bajajsales/product-details", {
                     method: "GET",
                 })
                 const result = await response.json()
